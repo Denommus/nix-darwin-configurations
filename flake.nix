eb4fa586240d2237e23eb4bf8d53386808baad9a
Yuri Albuquerque <yuridenommus@gmail.com>
2022-04-04 12:31:01 -0300

    Initial darwin configuration

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "John's darwin system";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-20.09-darwin";
+    darwin.url = "github:lnl7/nix-darwin/master";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nur.url = "github:nix-community/NUR";
+    nur.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, darwin, nixpkgs, nur, home-manager, ... }@inputs:
+  let
+    system = "x86_64-darwin";
+    nur-no-pkgs = import nur {
+      pkgs = null;
+      nurpkgs = import nixpkgs {
+        inherit system;
+      };
+    };
+  in {
+    darwinConfigurations."192" = darwin.lib.darwinSystem {
+      inherit system;
+      specialArgs = { inherit inputs; };
+      modules = [ ./darwin-configuration.nix ];
+    };
+  };
+}
