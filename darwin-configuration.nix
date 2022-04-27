3380e6084fe76974c709a655c8b9a2758a30ea97
Yuri Albuquerque <yuridenommus@gmail.com>
2022-04-27 18:53:25 -0300

    Add IOHK to my binary caches

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -32,6 +32,12 @@
     experimental-features = nix-command flakes
     auto-optimise-store = true
     '';
+    binaryCachePublicKeys = [
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+    binaryCaches = [
+      "https://hydra.iohk.io"
+    ];
   };
 
   programs.gnupg.agent = {
