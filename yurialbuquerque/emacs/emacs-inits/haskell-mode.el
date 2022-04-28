2ebdb463bb3636df7db05460b7214e6b20501d6f
Yuri Albuquerque <yuridenommus@gmail.com>
2022-04-28 16:14:03 -0300

    Configure lsp-haskell properly

--- yurialbuquerque/emacs/emacs-inits/haskell-mode.el
+++ yurialbuquerque/emacs/emacs-inits/haskell-mode.el
@@ -1,3 +1,9 @@
+(defun nix-haskell-sandbox-setup ()
+  (make-local-variable 'lsp-haskell-server-wrapper-function)
+  (setq lsp-haskell-server-wrapper-function
+        (lambda (argv)
+          (apply #'nix-shell-command (nix-current-sandbox) argv)))
+  (lsp))
 (add-hook 'haskell-mode-hook #'subword-mode)
-(add-hook 'haskell-mode-hook #'lsp)
-(add-hook 'haskell-literate-mode-hook #'lsp)
+(add-hook 'haskell-mode-hook #'nix-haskell-sandbox-setup)
+(add-hook 'haskell-literate-mode-hook #'nix-haskell-sandbox-setup)
--- yurialbuquerque/emacs/emacs-inits/rust-mode.el
+++ yurialbuquerque/emacs/emacs-inits/rust-mode.el
@@ -1,8 +1,8 @@
-(defun nix-sandbox-setup ()
+(defun nix-rust-sandbox-setup ()
   (make-local-variable 'lsp-rust-rls-server-command)
   (setq lsp-rust-rls-server-command (nix-shell-command (nix-current-sandbox) "rls"))
   (make-local-variable 'lsp-rust-analyzer-server-command)
   (setq lsp-rust-analyzer-server-command (nix-shell-command (nix-current-sandbox) "rust-analyzer"))
   (lsp))
 
-(add-hook 'rust-mode-hook #'nix-sandbox-setup)
+(add-hook 'rust-mode-hook #'nix-rust-sandbox-setup)
